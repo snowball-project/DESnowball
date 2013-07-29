960bee553f0ac017ad0e492befe89d27f1c0e138
yaomin <yaomin@gmail.com>
2013-07-28 20:06:01 -0500

    bug fix. toplist now print and return only gene names.

--- R/toplist.R
+++ R/toplist.R
@@ -1,9 +1,13 @@
 toplist <-
 function(fs)
-  ## toplist from RP feature selection algorithm
-  {
+    ## toplist from RP feature selection algorithm
+{
     ret <- fs$selectedList
-    names(ret) <- c('genes','FIT2','pvals','positive')
-    print(ret)
-    invisible(ret)
-  }
+    p.l <- row.names(subset(ret, subset=positive))
+    n.l <- row.names(subset(ret, subset=!positive))
+    cat("postive set:\n")
+    print(p.l)
+    cat("negative set: \n")
+    print(n.l)
+    invisible(list(positive=p.l,negative=n.l))
+}

a631f09a6788f73b800781e4978aaae7cb9328c0
yaomin <yaomin@gmail.com>
2013-07-28 19:02:00 -0500

    bug fix

--- R/start.para.R
+++ R/start.para.R
@@ -3,7 +3,9 @@ function(ncore, varlist, type="MPI") {
   if(ncore>1) cl <- makeCluster(getOption("cl.cores",ncore),
                                 type=type)
   else cl <- NULL
-  clusterExport(cl, varlist, envir=parent.frame())
+  clusterExport(cl, 
+		c("snowball.initexpr",varlist), 
+		envir=parent.frame())
   .dump <- clusterEvalQ(cl, eval(snowball.initexpr))
   ##.dump <- sfClusterEval(eval(initExpr))
   cl
